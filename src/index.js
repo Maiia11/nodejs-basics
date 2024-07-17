2d593bd1fc22879553a6b57c4e0558d953bce257
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-17 15:46:39 +0300

    fs methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 const message = 'Hello Node';
 
 console.log(message);
+
+
+import fs from 'fs/promises';
+
+// Записуємо дані у файл 'output.txt'
+(async () => {
+  const data = 'Це дані, які ми записуємо у файл.';
+  try {
+    await fs.writeFile('output.txt', data, 'utf8');
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+})();
+
+(async () => {
+  const data = 'Це дані, які ми додаємо до файлу.';
+  try {
+    await fs.appendFile('output.txt', data, 'utf8');
+    console.log('Дані успішно додані до файлу.');
+  } catch (err) {
+    console.error('Помилка додавання даних до файлу:', err);
+  }
+})();
+
+// Отримуємо список файлів і каталогів у поточному каталозі
+(async () => {
+  try {
+    const files = await fs.readdir('.');
+    console.log('Список файлів і каталогів:', files);
+  } catch (err) {
+    console.error('Помилка отримання списку файлів і каталогів:', err);
+  }
+})();
+(async () => {
+  const path = 'file.txt';
+  try {
+    await fs.access(path);
+    console.log(`Файл або каталог '${path}' доступний.`);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log(`Файл або каталог '${path}' не існує.`);
+    } else {
+      console.error(
+        `Помилка перевірки доступності файлу або каталогу '${path}':`,
+        err,
+      );
+    }
+  }
+})();
