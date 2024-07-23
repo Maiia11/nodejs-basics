4833821b84903d0b6ce0879ba4323e1a47c81630
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-07-23 20:44:56 +0300

    update code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import express from 'express';
-
-const app = express();
+import pino from 'pino-http';
+import cors from 'cors';
 
 const PORT = 3000;
 
+const app = express();
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello world!',
+    message: 'Hello World!',
   });
 });
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
